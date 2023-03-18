07ba366d460925a683a8ea77ba32a3b983498ce8
Akbar E K <akbar.e@cubettech.com>
2023-03-19 01:09:08 +0530

    google maps render

---
+++ src/Map.js
@@ -0,0 +1,68 @@
+import { useRef, useEffect } from "react";
+import html2canvas from "html2canvas";
+
+const Map = ({ setImage }) => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    // Load the Google Maps API script
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Initialize the map
+    window.initMap = () => {
+      new window.google.maps.Map(document.getElementById("map"), {
+        center: { lat: 37.7749, lng: -122.4194 }, // San Francisco, CA
+        zoom: 12,
+        disableDefaultUI: true
+      });
+    };
+
+    // Clean up the Google Maps API script
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  const takeScreenshot = () => {
+    html2canvas(mapRef.current, { useCORS: true }).then((canvas) => {
+      var cropLeft = 0;
+      var cropTop = 0;
+      var cropWidth = mapRef.current.offsetWidth;
+      var cropHeight = mapRef.current.offsetHeight;
+      var croppedCanvas = document.createElement("canvas");
+      croppedCanvas.width = cropWidth;
+      croppedCanvas.height = cropHeight;
+      croppedCanvas
+        .getContext("2d")
+        .drawImage(
+          canvas,
+          cropLeft,
+          cropTop,
+          cropWidth,
+          cropHeight,
+          0,
+          0,
+          cropWidth,
+          cropHeight
+        );
+      setImage(croppedCanvas.toDataURL());
+    });
+  };
+
+  return (
+    <div>
+      <div
+        id="map"
+        ref={mapRef}
+        style={{ height: "400px", width: "400px" }}
+      ></div>
+      <button id="screenshot-btn" onClick={takeScreenshot}>Take Screenshot</button>
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/Render.js
@@ -0,0 +1,132 @@
+import {
+  Engine,
+  ArcRotateCamera,
+  HemisphericLight,
+  MeshBuilder,
+  Scene,
+  Vector3,
+  StandardMaterial,
+  Texture,
+} from "@babylonjs/core";
+
+import React, { useEffect, useRef } from "react";
+
+const SceneComponent = (props) => {
+  const reactCanvas = useRef(null);
+
+  const {
+    canvasId,
+    antialias,
+    engineOptions,
+    adaptToDeviceRatio,
+    sceneOptions,
+    onRender,
+    onSceneReady,
+    ...rest
+  } = props;
+
+  useEffect(() => {
+    if (!reactCanvas.current) return;
+
+    const engine = new Engine(
+      reactCanvas.current,
+      antialias,
+      engineOptions,
+      adaptToDeviceRatio
+    );
+
+    const scene = new Scene(engine, sceneOptions);
+
+    if (scene.isReady()) {
+      onSceneReady(scene);
+    } else {
+      scene.onReadyObservable.addOnce(onSceneReady);
+    }
+
+    engine.runRenderLoop(() => {
+      onRender(scene);
+      scene.render();
+    });
+
+    const resize = () => {
+      scene.getEngine().resize();
+    };
+
+    if (window) {
+      window.addEventListener("resize", resize);
+    }
+
+    return () => {
+      scene.getEngine().dispose();
+
+      if (window) {
+        window.removeEventListener("resize", resize);
+      }
+    };
+  }, [
+    antialias,
+    engineOptions,
+    adaptToDeviceRatio,
+    sceneOptions,
+    onRender,
+    onSceneReady,
+  ]);
+
+  return <canvas id={canvasId} ref={reactCanvas} {...rest} />;
+};
+
+let box;
+
+const onSceneReady = (scene, image) => {
+  const camera = new ArcRotateCamera(
+    "Camera",
+    (3 * Math.PI) / 2,
+    Math.PI / 2,
+    5,
+    Vector3.Zero(),
+    scene
+  );
+
+  camera.setTarget(Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  camera.attachControl(canvas, true);
+
+  const light = new HemisphericLight("light", new Vector3(1, 1, 1), scene);
+
+  light.intensity = 0.7;
+
+  box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+  box.position.y = 0;
+
+  const material = new StandardMaterial("material", scene);
+  const texture = new Texture(image, scene);
+  material.diffuseTexture = texture;
+
+  box.material = material;
+};
+
+const onRender = (scene) => {
+  if (box !== undefined) {
+    var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+const Render = ({ image }) => (
+  <div>
+    <SceneComponent
+      canvasId="babylon-canvas"
+      antialias
+      onSceneReady={(scene) => onSceneReady(scene, image)}
+      onRender={onRender}
+    />
+  </div>
+);
+
+export default Render;
